a08363337dc55127e94e331861a0bf1ab0c605dd
Faqih <faqih0776@gmail.com>
2022-01-22 08:48:10 +0700

    Dasar sintaksis

---
+++ Dasar Syintaxis.py
@@ -0,0 +1,13 @@
+# Sekuensial
+jumlah_botol_susu = 150
+jumlah_telur = 20
+
+if jumlah_botol_susu > 0:
+    if jumlah_telur > 6:
+        print('Budi beli susu 1 botol dan beli telur 6 butir')
+    else:
+        print('Budi hanya beli 1 botol susu')
+
+
+else:
+    print('Tidak beli botol susu')
