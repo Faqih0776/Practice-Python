cf62bf629ac0c15af55be75603f242696ee7d1de
Faqih <faqih0776@gmail.com>
2022-01-22 09:20:16 +0700

    Fungsi For

---
+++ Fungsi FOR.py
@@ -0,0 +1,6 @@
+jumlah_buku = 10
+jumlah_buku_dibaca = 0
+print(f"jumlah buku yang sudah dibaca {jumlah_buku_dibaca}")
+for jumlah_buku_dibaca in range(1, jumlah_buku+1):
+    print(f"Buku ke {jumlah_buku_dibaca} yang sudah dibaca ")
+print(f"jumlah buku yang sudah dibaca {jumlah_buku_dibaca}")
